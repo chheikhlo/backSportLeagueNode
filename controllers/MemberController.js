2c946b2566371357e081a15e8d85f763b5f1250b
cheikh lo <cheikh.lo@anycommerce.fr>
2023-10-26 00:29:42 +0200

    some change on Member routers and Member controller

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -1,4 +1,4 @@
 
 
 // - getBasket()
-// - addProductInBasket()
+// - addProductToHisBasket()
--- routes/MemberRoutes.js
+++ routes/MemberRoutes.js
@@ -4,7 +4,7 @@ const routerMember = require('express').Router()
 // - GET : '/:id' : avec l'id du user en ligne on cherche le user dans les adherants et on liste ses produits dans son panier. 
 routerMember.get('/:id', MemberController.getBasket())
 
-// - POST : '/add/product' : le member ajoute un produit au panier
-routerMember.post('/add/product', MemberController.addProductInBasket())
+// - PUT : '/add/product' : le member ajoute un produit au panier
+routerMember.put('/add/product', MemberController.addProductToHisBasket())
 
 module.exports = router;
